b2fac06acc7cac8a06bc570fbb163ab0de53b990
FooOperator <foxoperator1@gmail.com>
2022-09-06 16:43:53 +0100

    debounce is fully functional
    changed params, separating required from optional.

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,27 +1,25 @@
 import { useEffect, useState } from "react";
 
-interface UseDebouceProps {
-  delay: number;
-  callback: Function;
-  options?: {
-    startActive?: boolean;
-  }
+interface UseDebounceOptions {
+  startActive?: boolean;
 }
 
-export default function useDebouce({ delay, callback, options }: UseDebouceProps) {
-  const [active, setActive] = useState<boolean>(options.startActive ?? true);
+export default function useDebounce(delay: number, callback: Function, options?: UseDebounceOptions) {
+  const [active, setActive] = useState<boolean>(options?.startActive ?? true);
   const [timer, setTimer] = useState<NodeJS.Timeout>();
   const toggleActive = () => setActive(prev => !prev);
 
   useEffect(() => {
-    setTimer(setTimeout(() => {
-      if (active) {
-        callback()
-      }
-    }, delay));
-
-    return () => clearTimeout(timer);
+    return () => clearInterval(timer);
   }, []);
+
+  useEffect(() => {
+    if (!active) {
+      clearInterval(timer);
+    } else {
+      setTimer(setInterval(() => callback(), delay));
+    }
+  }, [active]);
 
 
   return { active, toggleActive };
