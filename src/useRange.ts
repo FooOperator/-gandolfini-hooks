b8e44955a26cdc903b87d4a09d8aab238d4f719f
FooOperator <foxoperator1@gmail.com>
2022-09-17 18:15:27 +0100

    new funcs
    - wrap value when it reaches limit
    - value state within range
    - documented return

--- src/useRange.ts
+++ src/useRange.ts
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useToggle from "./useToggle";
 
 /**
- * Allows you to create a range, and check if a numerical value is inside of it (exclusive or inclusive).
- * @param start
- * @param end
+ * Allows you to create a numerical range, and check if a numerical value is inside of it (exclusive or inclusive).
+ * @param start start of range
+ * @param end end of range
+ * @param value current value inside range
  * @returns range object and verification functions.
  */
 export default function useRange(start: number, end: number) {
 	const [range] = useState<{ start: number; end: number }>({
 		end,
 		start,
+	});
+	const { value: wrapValue, toggle: toggleWrapValue } = useToggle();
+	const [value, setValue] = useState<number>(() => {
+		if (end > start) {
+			return end / start;
+		}
+
+		return start / end;
 	});
 
 	const insideRange = (value: number) => {
@@ -20,5 +30,45 @@ export default function useRange(start: number, end: number) {
 		return value > start && value < end;
 	};
 
-	return { ...range, insideRange, insideRangeExclusive };
+	useEffect(() => {
+		if (wrapValue) {
+			if (value > end) {
+				setValue(end);
+			} else if (value < start) {
+				setValue(start);
+			}
+		}
+
+	}, [value, wrapValue]);
+
+	return {
+		/**
+		 * Object containing the start and end of the range.
+		 */
+		...range,
+		/**
+		 * Set the current value to a new value.
+		 */
+		setValue,
+		/**
+		 * The state of the value.
+		 */
+		value,
+		/**
+		 * If true, whenever the value goes over or under the range boundaries, it's automatically brought back the either limit.
+		 */
+		wrapValue,
+		/**
+		 * Toggles the wrap value
+		 */
+		toggleWrapValue,
+		/**
+		 * Checks whether the provided value is within range.
+		 */
+		insideRange,
+		/**
+		 * Checks whether the provided value is within range and different from the limits.
+		 */
+		insideRangeExclusive
+	};
 }
